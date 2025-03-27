b27dd9a37354c753a781ae458ae949cff4e3b629
za23aee <za23aee@herts.ac.uk>
2025-03-27 16:45:30 +0000

    commit test

--- ShipState.java
+++ ShipState.java
@@ -7,6 +7,34 @@ import java.io.*;
  * @author A.Marczyk
  * @version 12/02/2025
  */
+
+     
+ private String name;
+ private String captain;
+ private int battleskill;
+ private int commissionFee;
+ private String status;
+ 
+ public Ship (String name, String captain, int battleskill, int commissionFee, String status){
+     name = this.name;
+     captain = this.captain;
+     battleskill = this.battleskill;
+     commissionFee = this.commissionFee;
+     status = this.status;  
+ }
+ 
+ 
+ //Acessors here 
+ 
+ public String getShipName()
+  {
+      return name;
+  }
+ 
+ //mutators here
+ 
+
+
 public enum ShipState implements Serializable
 {
     RESERVE(" In reserve fleet"), ACTIVE(" Active in squadron"), RESTING(" Resting"), SUNK (" Sunk");
